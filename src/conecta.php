8b00d0433906f60a6375454919273be562a08e52
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-05-30 10:45:33 -0300

    alterações

---
+++ src/conecta.php
@@ -0,0 +1,33 @@
+<?php
+
+/* Script de conexão ao Banco de Dados */
+
+/* Parâmetros */ 
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "crud_escola_lucas";
+
+
+try {
+
+  // Criando a Conexão com o MySQL (API/Driver de conexão)
+
+  $conexao = new PDO (
+    "mysql:host=$servidor; dbname=$banco; charset=utf8",
+    $usuario,
+    $senha
+);    
+
+// Habilitação a verificação de erros
+
+$conexao->setAttribute (
+    PDO::ATTR_ERRMODE, // Constante de erros em geral
+    PDO::ERRMODE_EXCEPTION // Constante de exceções de erro
+
+} catch (Exception $erro) {
+    die("Erro: " .$erro->getMessage());
+}
+
+var_dump($conexao); //teste
+?>
